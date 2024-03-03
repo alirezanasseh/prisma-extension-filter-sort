09f8df7b25968af96bcb70104fccd5cd9e15d4f6
alireza <alireza.nasseh@gmail.com>
2024-03-03 23:18:48 +0100

    Comment out the NestJs DTO file

--- src/types/rest/find-many.dto.ts
+++ src/types/rest/find-many.dto.ts
@@ -1,36 +1,38 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsEnum, IsOptional, IsString } from 'class-validator';
-import { IOrder } from '../sort-util.type';
+// You can use this file to define the DTO for the findMany method in the REST API in the NestJS project.
 
-export class FindManyParamsDto {
-  @ApiPropertyOptional({
-    description:
-      'Search query (JSON stringified)\n\n' +
-      'String modifiers: startsWith, endsWith, contains\n\n' +
-      'Number & Date modifiers: gt, gte, lt, lte\n\n' +
-      '{"name": "John", "email.contains": "@gmail.com", "id": 1, "createdAt.gte": "2021-01-01"}',
-  })
-  @IsOptional()
-  @IsString()
-  search?: string;
-
-  @ApiPropertyOptional()
-  @IsOptional()
-  page?: number;
-
-  @ApiPropertyOptional()
-  @IsOptional()
-  perPage?: number;
-
-  @ApiPropertyOptional()
-  @IsOptional()
-  @IsString()
-  sort?: string;
-
-  @ApiPropertyOptional({
-    enum: IOrder,
-  })
-  @IsOptional()
-  @IsEnum(IOrder)
-  order?: IOrder;
-}
+// import { ApiPropertyOptional } from '@nestjs/swagger';
+// import { IsEnum, IsOptional, IsString } from 'class-validator';
+// import { IOrder } from '../sort-util.type';
+//
+// export class FindManyParamsDto {
+//   @ApiPropertyOptional({
+//     description:
+//       'Search query (JSON stringified)\n\n' +
+//       'String modifiers: startsWith, endsWith, contains\n\n' +
+//       'Number & Date modifiers: gt, gte, lt, lte\n\n' +
+//       '{"name": "John", "email.contains": "@gmail.com", "id": 1, "createdAt.gte": "2021-01-01"}',
+//   })
+//   @IsOptional()
+//   @IsString()
+//   search?: string;
+//
+//   @ApiPropertyOptional()
+//   @IsOptional()
+//   page?: number;
+//
+//   @ApiPropertyOptional()
+//   @IsOptional()
+//   perPage?: number;
+//
+//   @ApiPropertyOptional()
+//   @IsOptional()
+//   @IsString()
+//   sort?: string;
+//
+//   @ApiPropertyOptional({
+//     enum: IOrder,
+//   })
+//   @IsOptional()
+//   @IsEnum(IOrder)
+//   order?: IOrder;
+// }
