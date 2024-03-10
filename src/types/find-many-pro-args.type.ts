fe7972ea5d88821e0e69e4aee7aabc6d17a7c418
alireza <alireza.nasseh@gmail.com>
2024-03-10 01:19:13 +0100

    removed extra types

--- src/types/find-many-pro-args.type.ts
+++
@@ -1,9 +0,0 @@
-import { IOrder } from './sort-util.type';
-
-export interface IFindManyProArgs {
-  search?: string;
-  sort?: string;
-  order?: IOrder;
-  page?: number;
-  perPage?: number;
-}
--- src/types/find-many-pro-ext-args.type.ts
+++
@@ -1,3 +0,0 @@
-import { IFindManyUtilArgs } from './find-many-util-args.type';
-
-export type IFindManyProExtArgs<T, K> = Omit<IFindManyUtilArgs<T, K>, 'model'>;
